463a218107c4bc22fadadfe31d7af49c4e5379ad
Joseph McGrath <joseph.mcgrath@engineer.com>
2020-10-02 12:33:00 +0100

    Extension working

--- app.js
+++ app.js
@@ -46,56 +46,97 @@ const alphabet = {
   ",":"--··--"
 };
 
+var morse = {
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  "/": " ",
+  "-·-·--": "!",
+  "·-·-·-": ".",
+  "--··--": ",",
+};
+
 class translator {
+  constructor(alphabet, morse) {
+    this.alphabet = alphabet;
+    this.morse = morse;
+    this.message = "";
+    this.translatedMessage = "";
+  }
 
-    constructor (alphabet) {
-        this.alphabet = alphabet;
-        this.message = "";
-        this.translatedMessage = "";
+  translateMorse() {
+    let message = document.getElementById("input");
+    let inputText = message.value.toLowerCase();
+    if (inputText.length === 0) {
+      console.log("Please write a message to translate");
+    } else {
+      // Split the message into an array
+      let input_array = inputText.split("");
+      // Map the input array onto the output array but making sure to convert on the way
+      let output_array = input_array.map((letter) => convert(letter));
+      output_array = output_array.join(" ");
+      document.getElementById("output").innerHTML = output_array;
+
+      function convert(letter) {
+        // 1. For each letter, return the output of this.alphabet."letter"
+        return alphabet[letter];
+      }
     }
+  }
 
+  translateEnglish() {
+    let message = document.getElementById("input");
+    let inputText = message.value;
+    if (inputText.length === 0) {
+      console.log("Please write a message to translate");
+    } else {
+      // Split the message into an array of individual letters based on spaces
+      let input_array = inputText.split(" ");
+      console.log(input_array);
+      // Map the input array onto the output array but making sure to convert on the way
+      let output_array = input_array.map((letter) => convert(letter));
+      output_array = output_array.join("");
+      document.getElementById("output").innerHTML = output_array;
 
-    translateMorse() {
-        let message = document.getElementById("input");
-        let inputText = message.value.toLowerCase();
-        if (inputText.length === 0) {
-            console.log("Please write a message to translate")
-        }
-        else {
-            // Split the message into an array
-            let input_array = inputText.split("");
-            // Map the input array onto the output array but making sure to convert on the way
-            let output_array = input_array.map( (letter) => convert(letter));
-            output_array = output_array.join(" ");
-            document.getElementById("output").innerHTML = (output_array);
-
-            function convert(letter) {
-                // 1. For each letter, return the output of this.alphabet."letter"
-                return alphabet[letter]
-            }
-        }
+      function convert(letter) {
+        // 1. For each letter, return the output of this.alphabet."letter"
+        return morse[letter];
+      }
     }
-
-    translateEnglish() {
-        let message = document.getElementById("input");
-        let inputText = message.value;
-        if (inputText.length === 0) {
-            console.log("Please write a message to translate")
-        }
-        else {
-          // Split the message into an array of individual letters based on spaces
-          let input_array = inputText.split(" ");
-          // Map the input array onto the output array but making sure to convert on the way
-          let output_array = input_array.map((letter) => convert(letter));
-          output_array = output_array.join("");
-          document.getElementById("output").innerHTML = output_array;
-
-          function convert(letter) {
-            // 1. For each letter, return the output of this.alphabet."letter"
-            return alphabet[letter];
-          }
-        }
-    }
+  }
 }
 
     const app = new translator(alphabet);
